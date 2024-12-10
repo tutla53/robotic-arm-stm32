6d1ec537a0a6c0d580ae704915c4706d8df07cdc
tutla <ayatullah_tutla@yahoo.com>
2024-12-10 14:16:41 +0700

    Create UART Remote Control Program

--- rtos/opencm3.d
+++ rtos/opencm3.d
@@ -1,19 +1,19 @@
 rtos/opencm3.o: rtos/opencm3.c rtos/FreeRTOS.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h FreeRTOSConfig.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h FreeRTOSConfig.h \
  rtos/projdefs.h rtos/portable.h rtos/deprecated_definitions.h \
  rtos/portmacro.h rtos/mpu_wrappers.h rtos/task.h rtos/list.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/rcc.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/cm3/common.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/memorymap.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/cm3/memorymap.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/f1/rcc.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/gpio.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/f1/gpio.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/cm3/nvic.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/dispatch/nvic.h \
- /mnt/c/Users/Formulatrix/Documents/PyRepo/stm32f1/libopencm3/include/libopencm3/stm32/f1/nvic.h
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/cm3/nvic.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /home/hades/stm32/libopencm3/include/libopencm3/stm32/f1/nvic.h
